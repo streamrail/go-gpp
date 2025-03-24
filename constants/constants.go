62810a57b5273971df0a2100f31319fa8d7db0ff
Inna Yaretsky <>
2025-03-24 16:25:39 +0200

    added section 5

--- constants/constants.go
+++ constants/constants.go
@@ -3,23 +3,24 @@ package constants
 type SectionID int
 
 const (
-	SectionTCFEU2 SectionID = 2
-	SectionGPP    SectionID = 3
-	SectionUSPV1  SectionID = 6
-	SectionUSPNAT SectionID = 7
-	SectionUSPCA  SectionID = 8
-	SectionUSPVA  SectionID = 9
-	SectionUSPCO  SectionID = 10
-	SectionUSPUT  SectionID = 11
-	SectionUSPCT  SectionID = 12
-	SectionUSPMT  SectionID = 14
-	SectionUSPOR  SectionID = 15
-	SectionUSPTX  SectionID = 16
-	SectionUSPDE  SectionID = 17
-	SectionUSPIA  SectionID = 18
-	SectionUSPNE  SectionID = 19
-	SectionUSPNH  SectionID = 20
-	SectionUSPNJ  SectionID = 21
+	SectionTCFEU2  SectionID = 2
+	SectionGPP     SectionID = 3
+	SectionTCFCAV1 SectionID = 5
+	SectionUSPV1   SectionID = 6
+	SectionUSPNAT  SectionID = 7
+	SectionUSPCA   SectionID = 8
+	SectionUSPVA   SectionID = 9
+	SectionUSPCO   SectionID = 10
+	SectionUSPUT   SectionID = 11
+	SectionUSPCT   SectionID = 12
+	SectionUSPMT   SectionID = 14
+	SectionUSPOR   SectionID = 15
+	SectionUSPTX   SectionID = 16
+	SectionUSPDE   SectionID = 17
+	SectionUSPIA   SectionID = 18
+	SectionUSPNE   SectionID = 19
+	SectionUSPNH   SectionID = 20
+	SectionUSPNJ   SectionID = 21
 )
 
 var SectionNamesByID = map[int]string{
